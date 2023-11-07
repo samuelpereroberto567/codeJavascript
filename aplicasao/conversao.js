659cc89d65d94a0ccd795a5a4e1b792f55754e67
samuelpereroberto567 <samuelmarinho3221@gmail.com>
2023-11-07 11:25:43 -0300

    consetra

--- aplicasao/conversao.js
+++ aplicasao/conversao.js
@@ -8,3 +8,13 @@ const numerostring = Number ("456");
 console.log(numero + Number (numerostring))
 // conversao explisita
 
+let minhaLet;
+console.log(minhaLet); //undefined
+
+// atribuindo um valor com TIPO DE DADO STRING
+minhaLet = "eu sou um texto";
+console.log(minhaLet); // "eu sou um texto"
+
+// reatribuindo, desta vez com dado do TIPO NUMBER
+minhaLet = 100;
+console.log(minhaLet); //100
