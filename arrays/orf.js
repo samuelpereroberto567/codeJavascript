fc2d16ab2028de7fbc72a4cf44b169fcb6532681
samuel7988 <samuelmarinho3221@gmail.com>
2023-11-25 17:33:45 -0300

    blmfb

---
+++ arrays/orf.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// Primeira expressão: é executada apenas uma única vez
+// Segunda expressão: condição de execução
+// Terceira expressão: é executada sempre ao final do bloco
+
+for (let indice = 6; indice < 6; indice++) {
+   // console.log("oi");
+   console.log(numeros[indice]);
+}
